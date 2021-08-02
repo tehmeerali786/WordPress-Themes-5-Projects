7a51c814f6897ef0b1fa458efb4970aa795c68dd
unknown <>
2021-08-01 18:49:19 -0700

    first commit

---
+++ simple/page.php
@@ -0,0 +1,46 @@
+
+
+	<?php get_header(); ?>
+
+	<div class="main">
+		<div class="container">
+		<?php if(have_posts()): ?>
+
+			<?php while(have_posts()): the_post(); ?>
+				<article class="post">
+				
+				<h3>
+					
+					<?php the_title(); ?>
+						
+					
+					</h3>
+				
+				
+
+				<?php if(has_post_thumbnail()): ?>
+
+					<div class="post-thumbnail">
+						<?php the_post_thumbnail(); ?>
+					</div>
+
+				<?php endif; ?>
+
+				<?php the_content(); ?>
+				</article>
+				<br>
+
+				
+			<?php endwhile; ?>
+
+		<?php else: ?>
+			<?php echo wpautop('Sorry, no posts were found') ?>
+		<?php endif; ?>
+
+
+		</div>
+
+	</div>
+
+
+	<?php get_footer(); ?>
