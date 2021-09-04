a19fdfb1e7cae33da7d0e024ff0ed3d7fa5ec8ca
unknown <>
2021-09-04 02:44:35 -0700

    Chapter 4

---
+++ wordstrap/footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+
+		<hr>
+		<footer>
+			<p>&copy; 2021 WordStrap</p>
+		</footer>
+		
+	</div>
+
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
+
+</body>
+</html>
